927d6cf3c4c0df825c45f8b3735b55ff6475c3f3
Minji Kim <qwe38280@gmail.com>
2022-10-31 13:23:52 +0900

    QueryDsl 추가

--- domain-user/src/main/java/com/kurrant/multi/repository/UserQuerydslRepository.java
+++ domain-user/src/main/java/com/kurrant/multi/repository/UserQuerydslRepository.java
@@ -1,12 +1,18 @@
 package com.kurrant.multi.repository;
 
+import com.kurrant.multi.domain.QUser;
 import com.kurrant.multi.domain.User;
+import com.kurrant.multi.dto.UserDto;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
 
+@Slf4j
 @Repository
 public class UserQuerydslRepository {
     private final EntityManager em;
@@ -15,6 +21,26 @@ public class UserQuerydslRepository {
     public UserQuerydslRepository(EntityManager em) {
         this.em = em;
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<UserDto> findByUsername(String name) {
+        QUser user = QUser.user;
+        List<UserDto> result = queryFactory
+                .select(Projections.fields(UserDto.class,
+                        user.role,
+                        user.name,
+                        user.point
+                ))
+                .from(user)
+                .where(user.name.eq(name))
+                .limit(10)
+                .fetch();
+        result.forEach(e -> {
+            log.info("role: " + e.getRole());
+            log.info("name: " + e.getName());
+            log.info("point: " + e.getPoint());
+        });
+        return result;
     }
 
     @Transactional
